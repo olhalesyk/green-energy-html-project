602b97e8ae2ea85a39951f790b1cec02f293b5d6
olhalesyk <olha.lesyk@powercoders.org>
2024-05-12 19:24:40 +0200

    Add the main home page layout

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./js/**/*.js", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        13: "repeat(13, minmax(100px, 1fr))",
+        14: "repeat(14, minmax(100px, 1fr))",
+        14: "repeat(15, minmax(100px, 1fr))",
+        16: "repeat(16, minmax(100px, 1fr))",
+        17: "repeat(17, minmax(100px, 1fr))",
+      },
+      gridColumn: {
+        "span-13": "span 13 / span 13",
+        "span-14": "span 14 / span 14",
+        "span-15": "span 15 / span 15",
+        "span-16": "span 16 / span 16",
+        "span-17": "span 17 / span 17",
+      },
+      gridColumnStart: {
+        13: "13",
+        14: "14",
+        15: "15",
+        16: "16",
+        17: "17",
+      },
+      gridColumnEnd: {
+        13: "13",
+        14: "14",
+        15: "15",
+        16: "16",
+        17: "17",
+      },
+    },
+    container: {
+      center: true,
+    },
+    colors: {
+      primary: "#1D1F1E",
+      secondary: "#646A69",
+      lowContrast: "#929C9A",
+      greensection: "#022c22",
+    },
+    fontFamily: {
+      body: ['"Figtree"', "sans-serif"],
+    },
+  },
+  plugins: [
+    require("flowbite/plugin"),
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    "prettier-plugin-tailwindcss",
+  ],
+};
